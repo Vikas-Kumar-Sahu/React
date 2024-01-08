576d5001c3f44e6a822baa35aa2a349354a8140d
Vikas-Kumar-Sahu <vk5588088@gmail.com>
2024-01-08 17:07:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Imageslider from './components';
+function App() {
+
+  return (
+    <>
+    <h1>First React Project with vite.</h1>
+    <p>We are starting the React Project 20/12/2023.</p><br />
+
+    </>
+  )
+}
+
+export default App;
---
+++ src/components.js
@@ -0,0 +1,12 @@
+
+const data = ["https://www.pexels.com/search/free%20wallpaper/","https://www.pexels.com/search/free%20wallpaper/","https://www.freepik.com/free-photos-vectors/wallpaper"];
+
+const Imageslider = () => {
+    return(
+        <>
+        <img src= {data} alt="wallpapers" />
+        </>
+    )
+}
+
+export default Imageslider;
