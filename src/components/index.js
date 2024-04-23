852d05b75ac453bffa35256f5e14297f9c0db177
Vikas-Kumar-Sahu <vk5588088@gmail.com>
2024-04-23 13:41:10 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Container from './container/Container';
+import Logo from './Logo';
+import LogoutBtn from './Header/LogoutBtn';
+import Login from './Login';
+import Button from './Button';
+import Input from './Input';
+import PostCard from './PostCard';
+import RTE from './RTE';
+import AuthLayout from './AuthLayout';
+import PostForm from './post-form/PostForm';
+import Select from './Select';
+import Signup from './Signup';
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Login,
+    Button,
+    Input,
+    PostCard,
+    RTE,
+    AuthLayout,
+    PostForm,
+    Select,
+    Signup,
+}
